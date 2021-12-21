8595a257f70cd5d8e66e65f324cbc558778b8d6f
MrFortuner <90767932+MrFortuner@users.noreply.github.com>
2021-12-22 02:24:28 +0300

    Delete СРС directory

--- СРС/app/models/user.rb
+++
@@ -1,7 +0,0 @@
-class User < ApplicationRecord
-  has_many :labs
-
-  validates :first_name, presence: true, length: { maximum: 100 }
-  validates :last_name, presence: true, length: { maximum: 100 }
-  validates_format_of :email, with: URI::MailTo::EMAIL_REGEXP
-end
--- СРС/config/routes.rb
+++
@@ -1,9 +0,0 @@
-Rails.application.routes.draw do
-  root 'labs#index'
-
-  resources :labs
-
-  get '/labs/:id/mark', to: 'labs#mark', as: 'mark_lab'
-
-  post '/labs/:id/mark', to: 'labs#grade'
-end
